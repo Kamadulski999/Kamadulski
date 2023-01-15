a6eab2d719e9b05269b4cb2de74c6b051c800f3f
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2023-01-15 12:20:20 -0600

    css updates for AWS deployment

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -38,7 +38,7 @@ function Nav() {
               </ul> 
               </div>             
          
-            <div className={'menu-icon'} onClick={handleClick}>
+            <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> 
             </div>
            
